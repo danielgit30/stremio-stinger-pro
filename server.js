89ad36befc15d3c486b803fe7c26c3ddb25669ad
Chief Keefe <schultz911@gmail.com>
2026-05-08 15:07:01 +0530

    Fixing vercel deployment 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 // --- Global Config & Helpers ---
 const config = {
     headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' },
-    timeout: 8000
+    timeout: 15000
 };
 
 const mapStatus = (mid, post, no) => {
