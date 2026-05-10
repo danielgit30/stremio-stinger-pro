0905df9f2dab53abe3472eba99e7b425c78ffdb1
Chief Keefe <schultz911@gmail.com>
2026-05-10 11:41:47 +0530

    Fixes.

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ async function checkAfterCredits(title, year) {
         });
 
         const pText = $$('article p, .entry-content p, #main p').text().toLowerCase();
-        if (pText.match(/\b(bloopers?|outtakes?)\b/)) {
+        if (pText.match(/\b(blooper?|outtake?)\b/)) {
             bloopers = true;
         }
 
@@ -214,7 +214,7 @@ async function checkMediaStinger(title, year) {
             const $$ = cheerio.load(movieRes.data);
             const pText = $$('article p, #content p, .post-content p, #main p').text().toLowerCase();
             
-            if (pText.match(/\b(bloopers?|outtakes?)\b/)) {
+            if (pText.match(/\b(blooper?|outtake?)\b/)) {
                 bloopers = true;
                 hasMid = false; 
             }
