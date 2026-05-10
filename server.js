8bd80ec7372b9b68666aa435447b200695a108fc
Chief Keefe <schultz911@gmail.com>
2026-05-11 03:35:56 +0530

    Complete rewrite. No version bump. Final. 2.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 const config = {
     headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36' },
-    timeout: 8000 
+    timeout: 15000
 };
 const DEFAULT_TMDB_KEY = "dc0aefa944df1ef858fafd8085d2e60f"; 
 
