e0558fe36afb66128b56f9dd54abdda981ace886
Chief Keefe <schultz911@gmail.com>
2026-06-03 23:04:02 +0530

    Release version 2.2: WIP

--- src/scrapers/wikipedia.js
+++ src/scrapers/wikipedia.js
@@ -84,6 +84,7 @@ async function buildWikiIndex() {
                 redisCache.setCache('wiki_index_cache', flatData, Math.floor(WIKI_TTL / 1000));
                 log(`[Wiki] Saved pre-compiled index to Redis cache.`);
             }
+
         } catch (e) {
             if (e.name !== 'CanceledError' && e.message !== 'canceled') {
                 console.error(`[Wiki Error] ${sanitizeError(e.message)}`);
