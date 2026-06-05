eebf4252618ee4edd8c18ce9499bb303ccab8dcc
Chief Keefe <schultz911@gmail.com>
2026-06-06 02:20:32 +0530

    Version 3.0.10

--- src/routes/manifest.js
+++ src/routes/manifest.js
@@ -5,7 +5,7 @@ const manifestHandler = (req, res) => {
         version: '3.0.10',
         name: 'Stremio Stinger Pro',
         description:
-            'Detects mid/post-credit scenes and optionally bloopers/outtakes and sequel setups. Powered by a multi-tiered scraping system including AfterCredits, TMDB, and Wikipedia.',
+            'Instantly detect mid/post-credits scenes, outtakes, sequel setups, and extended franchise metadata (prequels, sequels, source material) in Stremio. Powered by AfterCredits, TMDb, and Wikipedia.',
         logo: 'https://raw.githubusercontent.com/schultz911/stremio-stinger-pro/main/public/icon.png?v=3.0.10',
         types: ['movie'],
         catalogs: [],
